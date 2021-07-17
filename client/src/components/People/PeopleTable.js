68cb60cac3f92a8df5ea7025c0e33579cfc17dd1
Jake Kaad <jkaad@POR-JKAADMAC03.local>
2021-07-17 08:50:34 -0700

    [PEOPLE_API] Display letter frequency Fixes some display issues with table Move Table styles to their own file

--- client/src/components/People/PeopleTable.js
+++ client/src/components/People/PeopleTable.js
@@ -1,47 +1,28 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Table, THead, HeaderCell, TableCell } from './TableStyles'
 
-const Table = styled.table`
-  color: #333;
-  background: white;
-  border: 1px solid grey;
-  font-size: 12pt;
-  border-collapse: collapse;
-`
-
-const THead = styled.thead`
-  color: #777;
-  background: rgba(0, 0, 0, 0.1);
-`
-
-const HeaderCell = styled.th`
-  padding: 0.5em;
-  border: 1px solid lightgrey;
-`
-
-const TableCell = styled.td`
-  padding: 0.5em;
-  border: 1px solid lightgrey;
-`
-
-const UsersTable = ({ users }) => (
+const PeopleTable = ({ people }) => (
   <Table>
     <THead>
-      <HeaderCell> Name </HeaderCell>
-      <HeaderCell> Email </HeaderCell>
-      <HeaderCell> Job Title </HeaderCell>
+      <tr>
+        <HeaderCell> Name </HeaderCell>
+        <HeaderCell> Email </HeaderCell>
+        <HeaderCell> Job Title </HeaderCell>
+      </tr>
     </THead>
-
-    {users
-      ? users.map((user, i) => (
-          <tr>
-            <TableCell> {user.name} </TableCell>
-            <TableCell> {user.email} </TableCell>
-            <TableCell> {user.job_title} </TableCell>
-          </tr>
-        ))
-      : null}
+    <tbody>
+      {people
+        ? people.map((user, i) => (
+            <tr key={i}>
+              <TableCell> {user.name} </TableCell>
+              <TableCell> {user.email} </TableCell>
+              <TableCell> {user.job_title} </TableCell>
+            </tr>
+          ))
+        : null}
+    </tbody>
   </Table>
 )
 
-export default UsersTable
+export default PeopleTable
--- client/src/components/People/index.js
+++ client/src/components/People/index.js
@@ -2,9 +2,30 @@ import React, { useState } from 'react'
 import styled from 'styled-components'
 import axios from 'axios'
 import PeopleTable from './PeopleTable'
+import Letters from './Letters'
+
+const Button = styled.button`
+  background-color: #4caf50; /* Green */
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+`
 
 const fetchPeople = (setPeople) =>
   axios.get('api/people').then((response) => setPeople(response.data))
+
+const renderPeopleSwitch = (param, { people }) => {
+  switch (param) {
+    case 'LetterCounter':
+      return <Letters people={people} />
+    default:
+      return <PeopleTable people={people} />
+  }
+}
 
 const People = () => {
   const [people, setPeople] = useState(null)
@@ -12,11 +33,13 @@ const People = () => {
 
   people || fetchPeople(setPeople)
 
-  return
-  ;<>
-    <button onClick={setPageSwitch('People')}>People </button>
-    <PeopleTable people={people} />
-  </>
+  return (
+    <div>
+      <Button onClick={() => setPageSwitch('People')}>People </Button>
+      <Button onClick={() => setPageSwitch('LetterCounter')}>Letters </Button>
+      {renderPeopleSwitch(pageSwitch, { people })}
+    </div>
+  )
 }
 
 export default People
