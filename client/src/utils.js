924022e798710d84a4859d9647d23490180ab81c
Jake Kaad <jkaad@POR-JKAADMAC03.local>
2021-07-17 08:12:17 -0700

    [PEOPLE_API] Create a reducer to count unique chars in an array of strings

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,1 +1,11 @@
-export const merge = (...obj) => Object.assign({}, ...obj);
+export const merge = (...obj) => Object.assign({}, ...obj)
+
+const countLetters = (letterCount, string) => {
+  const chars = [...string]
+  chars.forEach((char) =>
+    letterCount[char] ? (letterCount[char] += 1) : (letterCount[char] = 1)
+  )
+  return letterCount
+}
+
+export const letterCounter = (strings) => strings.reduce(countLetters, {})
